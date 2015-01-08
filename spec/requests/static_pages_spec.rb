0ebb00f873ee6e1a02fb5b29734ed65a83943815
lemboy <lemboy@gmail.com>
2015-01-08 21:17:19 +0300

    After chapter 5

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,28 +5,11 @@ describe "Static pages" do
   subject { page }
 
   describe "Home page" do
-<<<<<<< HEAD
-    it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('Sample App')
-    end
-
-    it "should have the base title" do
-      visit '/static_pages/home'
-      expect(page).to have_title("#{base_title}")
-    end
-
-    it "should not have a custom page title" do
-      visit '/static_pages/home'
-      expect(page).not_to have_title('| Home')
-    end
-=======
     before { visit root_path }
 
     it { should have_content('Sample App') }
     it { should have_title(full_title('')) }
     it { should_not have_title('| Home') }
->>>>>>> filling-in-layout
   end
 
   describe "Help page" do
